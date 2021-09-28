434466542400f0c024c1f5972f4483bdc613ddac
masohn <78728639+masohn@users.noreply.github.com>
2021-09-28 16:24:39 +0200

    adding convert.py

---
+++ convert.py
@@ -0,0 +1,34 @@
+# !/usr/bin/python
+from collections import Counter
+import forgi.graph.bulge_graph as cgb
+
+
+# INPUT:: Path to RNAfold file
+input = 'RNAfold/Dataset_B.fasta'
+
+# OUTPUT:: List of structures
+output = 'structures/Dataset_B.txt'
+
+dotlist = []
+structurelist = []
+
+# Speichert die Dot-Bracket Strukturen in eine Liste
+with open(input, 'r') as f:
+    for count, line in enumerate(f, start=1):
+        if count % 3 == 0:
+            line = line.split(" ")[0]
+            dotlist.append(line)
+
+# Konvertiert Dot-Bracket zu Element-String Strukturen
+for structures in dotlist:
+    bg = cgb.BulgeGraph.from_dotbracket(structures)
+    elementstring = bg.to_element_string()
+    important = elementstring[43:48]
+    structurelist.append(important)
+
+# Schreibt den Output
+outF = open(output, "w")
+for line in structurelist:
+  outF.write(line)
+  outF.write("\n")
+outF.close()
